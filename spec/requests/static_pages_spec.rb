9c31bc5d22c2f611d44f7f996f03b7faf828af23
adrenalin88 <alexsnder88.88@mail.ru>
2014-10-14 22:16:28 +0400

    Finish about action

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,11 @@ describe "StaticPages" do
   describe "About page" do
     it "should have text 'about'" do      
       visit '/static_pages/about'
-      expect(page).to have_content('About MoneyCalc')
+      expect(page).to have_content('About MoneyCalc')     
+    end
+    it "should have text 'users count'" do      
+      visit '/static_pages/about'      
+      expect(page).to have_content('Users count:')
     end
   end
 end
